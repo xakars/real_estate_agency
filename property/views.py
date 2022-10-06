3d0551b8ebcc032bde71e43575b6039cea4c6dfa
Arsen <xakars02@gmail.com>
2022-10-06 23:39:51 +0600

    Enable filter through new buildings on index page

--- property/views.py
+++ property/views.py
@@ -15,7 +15,7 @@ def show_flats(request):
     town = request.GET.get('town')
     min_price = format_price(request.GET.get('min_price'))
     max_price = format_price(request.GET.get('max_price'))
-    new_building = request.GET.get('new_building') == '1'
+    new_building = request.GET.get('new_building')
 
     flats = Flat.objects.all()
     if town:
@@ -24,6 +24,8 @@ def show_flats(request):
         flats = flats.filter(price__gt=min_price)
     if max_price:
         flats = flats.filter(price__lt=max_price)
+    if new_building:
+        flats = flats.filter(new_building=True)
 
     towns = Flat.objects.values_list(
         'town', flat=True).distinct().order_by('town')
