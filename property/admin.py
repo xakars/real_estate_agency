826dc74658893bd3a16898514d0c32260ce6446c
Arsen <xakars02@gmail.com>
2022-10-09 20:45:59 +0600

    set M2M relationship to flats filed

--- property/admin.py
+++ property/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Flat, Complain, Owner
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address', 'owner')
+    search_fields = ('town', 'address')
     readonly_fields = ["created_at"]
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owners_phonenumber', 'owner_pure_phone']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ["liked_by"]
--- property/models.py
+++ property/models.py
@@ -5,12 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        region="RU",
-        blank=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.BooleanField(null=True, blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
@@ -81,7 +75,8 @@ class Owner(models.Model):
     flats = models.ManyToManyField(
         Flat,
         related_name="owns",
-        verbose_name = 'Квартиры в собственности')
+        verbose_name = 'Квартиры в собственности',
+        db_index=True)
 
     def __str__(self):
         return f'{self.owner}'
