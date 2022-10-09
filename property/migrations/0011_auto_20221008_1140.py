f04f4b70b2090d7bc455844717b1250adede82dd
Arsen <xakars02@gmail.com>
2022-10-09 17:36:01 +0600

    fix previous data-migrations

--- property/migrations/0011_auto_20221008_1140.py
+++ property/migrations/0011_auto_20221008_1140.py
@@ -7,7 +7,14 @@ def copy_flatownwers_to_owners_model(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     Owner = apps.get_model('property', 'Owner')
     for flat in Flat.objects.all():
-        Owner.objects.get_or_create(owner=flat.owner)
+        Owner.objects.get_or_create(
+            owner=flat.owner,
+            owners_phonenumber=flat.owners_phonenumber,
+            owner_pure_phone=flat.owner_pure_phone)
+
+def move_backward(apps, schema_editor):
+    Owner = apps.get_model('property', 'Owner')
+    Owner.objects.all().delete()
 
 class Migration(migrations.Migration):
 
@@ -16,5 +23,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(copy_flatownwers_to_owners_model)
+        migrations.RunPython(copy_flatownwers_to_owners_model, move_backward)
     ]
