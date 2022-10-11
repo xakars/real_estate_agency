ba4401513102e729a6a6149a4bb85df2d1287d0f
Arsen <xakars02@gmail.com>
2022-10-11 18:57:35 +0600

    use @register decorator

--- property/admin.py
+++ property/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import Flat, Complain, Owner
 
+
 class OwnerFlatInline(admin.TabularInline):
     model = Owner.flats.through
     raw_id_fields = ["owner", "flat"]
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnerFlatInline]
     search_fields = ('town', 'address')
@@ -15,12 +17,11 @@ class FlatAdmin(admin.ModelAdmin):
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ["liked_by"]
 
+@admin.register(Complain)
 class ComplainAdmin(admin.ModelAdmin):
     raw_id_fields = ["complainant", "contested_flat"]
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ["flats"]
 
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complain, ComplainAdmin)
-admin.site.register(Owner, OwnerAdmin)
