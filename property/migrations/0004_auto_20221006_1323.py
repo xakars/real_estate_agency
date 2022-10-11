e5a49def495b195a0b2c35685f70d2b7669e2dde
Arsen <xakars02@gmail.com>
2022-10-11 19:17:54 +0600

    fix 0004 migration

--- property/migrations/0004_auto_20221006_1323.py
+++ property/migrations/0004_auto_20221006_1323.py
@@ -4,13 +4,7 @@ from django.db import migrations
 
 def update_new_building_field(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        else:
-            flat.new_building = False
-        flat.save()
-
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
 
 class Migration(migrations.Migration):
 
